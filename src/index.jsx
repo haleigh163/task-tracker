28a4a812a120eb1feda11fc7df217ab731410287
haleigh163 <68418954+haleigh163@users.noreply.github.com>
2022-06-15 06:01:07 -0600

    update index.jsx file
    add code for react 17; couldn't update to react 18

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,26 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-
+import React from "react";
+import ReactDOM from "react-dom";
 import App from "./App";
 
-const rootElement = document.getElementById("root");
-const root = createRoot(rootElement);
-
 ReactDOM.render(
-  <StrictMode>
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>,
   document.getElementById("root")
 );
+
+//? after update to react 18
+// import { StrictMode } from "react";
+// import { createRoot } from "react-dom/client";
+
+// import App from "./App";
+
+// const rootElement = document.getElementById("root");
+// const root = createRoot(rootElement);
+
+// ReactDOM.render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>,
+//   document.getElementById("root")
+// );
